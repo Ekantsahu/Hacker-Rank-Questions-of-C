82bd4347e4d724f58bf074dd190c65c2eff6f201
techueku <ekantsahu27@gmail.com>
2024-10-05 19:46:49 +0530

    Conditionals and Loops

---
+++ Conditionals and Loops/Solution.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Reading input values
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    // Loop from 'a' to 'b' inclusive
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            
+            switch (i) {
+                case 1: printf("one\n"); break;
+                case 2: printf("two\n"); break;
+                case 3: printf("three\n"); break;
+                case 4: printf("four\n"); break;
+                case 5: printf("five\n"); break;
+                case 6: printf("six\n"); break;
+                case 7: printf("seven\n"); break;
+                case 8: printf("eight\n"); break;
+                case 9: printf("nine\n"); break;
+            }
+        } else {
+           
+            if (i % 2 == 0) {
+                printf("even\n");
+            } else {
+                printf("odd\n");
+            }
+        }
+    }
+
+    return 0;
+}
